07daeb54b043182a68bd3dd1a4555bea59bc5f01
Skilled5041 <skilled5041@gmail.com>
2024-09-24 16:22:20 -0400

    Started hero section

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,5 +10,6 @@ export default defineConfig({
         tailwind({
             applyBaseStyles: false,
         }),
+        icon()
     ]
 });
